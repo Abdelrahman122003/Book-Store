b2956ca70e311628b7c867430747dd9ac313aa75
Mustafa Ahmed <mostafa3132004@gmail.com>
2024-02-08 00:39:59 +0200

    hotfix

--- backend/app.js
+++ backend/app.js
@@ -6,3 +6,4 @@ module.exports = app;
 
 
 
+
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 
 const port = 2003;
 
-app.listen(port , () => {
-    console.log(`I'm listeninig on port ${port}...`);
-});
+mongoose.set("strictQuery", false);
+mongoose
+    .connect('mongodb+srv://mostafa3132004:DuXaxkJrP8RGuXR2@cluster0.tyo186d.mongodb.net/')
+    .then(() => {
+        console.log('Database connected successfully!');
+        app.listen(port , () => {
+            console.log(`listeninig to port ${port}...`);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
