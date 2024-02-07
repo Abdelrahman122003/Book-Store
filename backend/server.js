7a994509e44ce052a1ea38e0d4c1e62e141f83a6
Mustafa Ahmed <mostafa3132004@gmail.com>
2024-02-08 01:34:38 +0200

    hotfix

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
+const DB = 'mongodb+srv://mostafa3132004:DuXaxkJrP8RGuXR2@cluster0.tyo186d.mongodb.net/';
+const DB1 = 'mongodb+srv://mostafa3132004:<DuXaxkJrP8RGuXR2>@cluster0.tyo186d.mongodb.net/?retryWrites=true&w=majority';
 const port = 2003;
 
 mongoose.set("strictQuery", false);
 mongoose
-    .connect('mongodb+srv://mostafa3132004:DuXaxkJrP8RGuXR2@cluster0.tyo186d.mongodb.net/')
+    .connect(DB1)
     .then(() => {
         console.log('Database connected successfully!');
         app.listen(port , () => {
