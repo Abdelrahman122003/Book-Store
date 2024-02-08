6083d5bf589aada1dd35eb63ab5b9e922dddbbde
Abdelhman-sayed <abdelrhmansayed122003@gmail.com>
2024-02-09 00:31:10 +0200

    Add property to book model

--- backend/models/book.js
+++ backend/models/book.js
@@ -1,23 +1,15 @@
 const mongoose = require("mongoose");
 const bookSchema = mongoose.Schema({
-  name: 
-  { type: String, required: true },
-  price: 
-  { type: Number, required: true },
-  amount: 
-  { type: Number, default: 1 },
-  author: 
-  { type: String, required: true },
-  ISBN: 
-  { type: String, required: true, unique: true },
-  pages: 
-  { type: Number, required: true },
-  cover: 
-  { type: String, required: true },
-  category: 
-  { type: String, required: true },
-  rate: 
-  { type: Number, default: 1 }
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  amount: { type: Number, default: 1 },
+  author: { type: String, required: true },
+  ISBN: { type: String, required: true, unique: true },
+  pages: { type: Number, required: true },
+  cover: { type: String, required: true },
+  category: { type: String, required: true },
+  rate: { type: Number, default: 1 },
+  numberOfPayments: { type: Number, default: 0 },
 });
 
 const Book = mongoose.model("book", bookSchema);
