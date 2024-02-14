605bbd4dc0d7100bb193dfcdea99562fffc8b3a2
Mustafa Ahmed <mostafa3132004@gmail.com>
2024-02-14 17:15:11 +0200

    Delete midlleware that check empty params

--- backend/routes/bookRouter.js
+++ backend/routes/bookRouter.js
@@ -2,14 +2,14 @@ const express = require("express");
 const routerBook = express.Router();
 const controllerBook = require("../controllers/controllerBook");
 const checkNullsBody = require("../middlewares/checkNulls");
-const {nullISBN} = require("../middlewares/checkNullsParam")
+// const {nullISBN} = require("../middlewares/checkNullsParam")
 
 // param in link ----> get, updatesd
 // requset body  ----> post, update
 routerBook.route("/addBook").post(checkNullsBody , controllerBook.addBook);
 routerBook.route("/showBooks").get(controllerBook.showBooks);
-routerBook.route("/deleteBook/:ISBN").delete(nullISBN, controllerBook.deleteBook);
-routerBook.route("/editBook/:ISBN").patch(nullISBN, checkNullsBody, controllerBook.editBook);
-routerBook.route("/getBookByISBN/:ISBN").get(nullISBN, controllerBook.getBookByISBN);
+routerBook.route("/deleteBook/:ISBN").delete(controllerBook.deleteBook);
+routerBook.route("/editBook/:ISBN").patch(checkNullsBody, controllerBook.editBook);
+routerBook.route("/getBookByISBN/:ISBN").get(controllerBook.getBookByISBN);
 
 module.exports = routerBook;
