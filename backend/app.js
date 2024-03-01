282f576a157e9afed98cc946e9f6fca4008720bf
Abdelhman-sayed <abdelrhmansayed122003@gmail.com>
2024-03-01 15:21:06 +0200

    Secure HTTP headers by utilizing the 'helmet' package

--- backend/app.js
+++ backend/app.js
@@ -7,31 +7,30 @@ const bookRouter = require("../backend/routes/bookRouter");
 const customerRouter = require("./routes/customerRouter");
 const orderRouter = require("./routes/orderRouter");
 const handlerErrorsAuto = require("./controllers/errorController");
-const app = express();
-app.use(bodyParser.json());
+const helmet = require("helmet");
 
-// app.use((err, req, res, next) => {
-//   // Handle the error
-//   res.status(err.statusCode || 500).json({
-//     status: err.status || "error",
-//     message: err.message,
-//   });
-// });
+const app = express();
 
 // **Global MIDDLEWARES
+
+// Set security http headers
+app.use(helmet());
+// body parser, reading data from body into req.body
+app.use(bodyParser.json({ limit: "10kb" }));
+
 // Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-// this limiter allow customer send http request max --> 100
+// **This limiter allows customers to send a maximum of 100 HTTP requests within the specified time window (windowMs).
 const limiter = rateLimiter({
   max: 10,
-  windowMs: 60 * 60 * 1000,
+  windowMs: 1 * 60 * 1000,
   message: "Too Many Requests from this IP, please try again in an hour",
 });
 
 app.use("/api", limiter);
-
+//
 app.use("/api/books", bookRouter);
 app.use("/api/customers", customerRouter);
 app.use("/api/orders", orderRouter);
